8616a761c122f0bfcd74e48085875c1b6828281a
jarryingnut <rabhishekranjan@gmail.com>
2022-12-29 16:57:23 +0530

    remove onChain test

--- test/Winner.js
+++ test/Winner.js
@@ -1,7 +1,7 @@
 const { expect, assert } = require("chai");
 
 describe("Test Winner Locally", function () {
-  it("Winner contrat emits winner event", async function () {
+  it("Winner contract emits winner event", async function () {
     const Winner = await hre.ethers.getContractFactory("Winner");
     const winner = await Winner.deploy();
     await winner.deployed;
@@ -13,23 +13,6 @@ describe("Test Winner Locally", function () {
     // await expect(winner.attempt()).to.be.revertedWith(
     //   "msg.sender is equal to tx.origin'"
     // );
-
-    await expect(contact.callAttempt(winner.address)).to.emit(winner, "Winner");
-  });
-});
-
-describe("Test Winner onChain", function () {
-  //Contact.sol 0xe8ee6004B97355a6E620fB563c0ED7Ef6B463b05
-  //Winner 0xcF469d3BEB3Fc24cEe979eFf83BE33ed50988502
-
-  it("Winner contrat emits winner event", async function () {
-    const Winner = await hre.ethers.getContractFactory("Winner");
-    const winner = await Winner.deploy();
-    await winner.deployed;
-
-    const Contact = await hre.ethers.getContractFactory("Contact");
-    const contact = await Contact.deploy();
-    await contact.deployed;
 
     await expect(contact.callAttempt(winner.address)).to.emit(winner, "Winner");
   });
